490c40b55721f4b7f943a53adfbeac5ab2ff4ec5
QASIM_DEvELOPER <mirzaq620@gamil.com>
2024-01-26 19:06:55 +0500

    changes added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
+import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin, AiFillYoutube, AiFillFacebook } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, ShowCase, SocialIcons } from './HeaderStyles';
@@ -36,8 +36,8 @@ const Header = () =>  (
         <SocialIcons href="https://www.linkedin.com/in/qasim-mirza-4a1860289/" target="_blank">
           <AiFillLinkedin size="3rem" />
         </SocialIcons>
-        <SocialIcons href="https://www.linkedin.com/in/qasim-mirza-4a1860289/" target="_blank">
-          <AiFillLinkedin size="3rem" />
+        <SocialIcons href="https://m.facebook.com/profile.php?id=100072096265847" target="_blank">
+          <AiFillFacebook size="3rem" />
         </SocialIcons>
      
       
